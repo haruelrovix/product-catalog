b4e02cd405d0f6efe2c4042c064ad8baa399669a
haruelrovix <havitchoirul.rovik@mitrais.com>
2018-03-20 14:31:21 +0700

    Get Catalogue based on given Amount and Offset

--- backend/mock/index.js
+++ backend/mock/index.js
@@ -8,8 +8,8 @@ const dejiesa = {
   quantity: 13,
   images: [
     {
-      id: 224196,
-      url: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/cfb83b40b45029b484d400af378e55cd.jpg',
+      // id: 224196,
+      fullUrl: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/cfb83b40b45029b484d400af378e55cd.jpg',
       width: 800,
       height: 800,
     },
@@ -34,8 +34,8 @@ const frixza = {
   category: 'Maxi Dress',
   images: [
     {
-      id: 227752,
-      url: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/caeffd8c014bf570f9e5633f96af0e48.jpg',
+      // id: 227752,
+      fullUrl: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/caeffd8c014bf570f9e5633f96af0e48.jpg',
       width: 800,
       height: 800,
     },
@@ -55,50 +55,50 @@ const galzinsa = {
   category: 'Mini Dress',
   images: [
     {
-      id: 227753,
-      url: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/60d4bd5f59a6c89028f84678cef6f3b2.jpg',
+      // id: 227753,
+      fullUrl: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/60d4bd5f59a6c89028f84678cef6f3b2.jpg',
       width: 800,
       height: 800,
     },
     {
-      id: 227754,
-      url: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/7ce7c0bd615a20033186de7d11475749.jpg',
+      // id: 227754,
+      fullUrl: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/7ce7c0bd615a20033186de7d11475749.jpg',
       width: 800,
       height: 800,
     },
     {
-      id: 227755,
-      url: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/e0e21d8b6fb8071197babebc89995009.jpg',
+      // id: 227755,
+      fullUrl: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/e0e21d8b6fb8071197babebc89995009.jpg',
       width: 800,
       height: 800,
     },
     {
-      id: 227756,
-      url: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/8ed0bd11f6c4878e889311e1a91cd0dc.jpg',
+      // id: 227756,
+      fullUrl: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/8ed0bd11f6c4878e889311e1a91cd0dc.jpg',
       width: 800,
       height: 800,
     },
     {
-      id: 227757,
-      url: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/652e63a21828ead238b34ee3bb52516d.jpg',
+      // id: 227757,
+      fullUrl: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/652e63a21828ead238b34ee3bb52516d.jpg',
       width: 800,
       height: 800,
     },
     {
-      id: 227758,
-      url: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/38cfc91ee13817f546adcf87146626fb.jpg',
+      // id: 227758,
+      fullUrl: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/38cfc91ee13817f546adcf87146626fb.jpg',
       width: 800,
       height: 800,
     },
     {
-      id: 227759,
-      url: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/fb14df572a946a5b97e8ec876d6c9f54.jpg',
+      // id: 227759,
+      fullUrl: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/fb14df572a946a5b97e8ec876d6c9f54.jpg',
       width: 800,
       height: 800,
     },
     {
-      id: 227760,
-      url: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/fa3b22cdd22efb1775ac0e606d634a14.jpg',
+      // id: 227760,
+      fullUrl: 'https://ss-imager-prod.freetls.fastly.net/www-images/480/product_images/fa3b22cdd22efb1775ac0e606d634a14.jpg',
       width: 800,
       height: 800,
     },
@@ -111,11 +111,16 @@ const items = [
   galzinsa,
 ].concat(raw);
 
+const product = {
+  items,
+  total: items.length,
+};
+
 const catalogue = {
   id: 'a2e458ad-28b3-11e5-8c52-061c7301af31',
   title: 'Dress',
   metaTitle: 'Baju Dress Gaun Wanita Model Panjang dan Pendek',
-  items,
+  product,
 };
 
 export default catalogue;
--- backend/resolvers/index.js
+++ backend/resolvers/index.js
@@ -4,9 +4,26 @@ const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
 
-    product: (_, { id }) => catalogue.items.find(product => product.id === id),
+    product: (_, { id }) => catalogue.product.items.find(product => product.id === id),
 
-    catalogue: () => catalogue,
+    catalogue: (_, { amount, offset }) => {
+      const begin = amount * offset;
+      const end = amount * (offset + 1);
+      const items = catalogue.product.items.slice(begin, end);
+
+      const product = {
+        ...catalogue.product,
+        items,
+        amount,
+        offset,
+        remaining: (catalogue.product.total - ((offset + 1) * amount)),
+      };
+
+      return {
+        ...catalogue,
+        product,
+      };
+    },
   },
 };
 
